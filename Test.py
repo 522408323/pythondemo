b8b36588102d2a3386525b7dbebd0ee1d24bb151
shenyafei1 <shenyafei@labifenqi.com>
2021-03-31 18:04:20 +0800

    测试

---
+++ Test.py
@@ -0,0 +1,31 @@
+
+if __name__ == '__main__':
+
+#     print("111111")
+#     print(5+10)
+# print("hello" + " world")
+# print("love" * 3)
+# print("love\n" * 3)
+# 单行注释： #  ,多行注释： ctrl + / 或者 3个单引号 或者 3个双引号 ,多行注释貌似需要先空tab行,不然后面代码报错
+    '''
+    temp =========
+    '''
+# 使用变量前需要先赋值
+#print(a)
+
+# 变量名：可以是字符，数字，下划线，不能以数字开头
+    print(8)
+# 变量名区分大小写
+    a = "a1"
+    A = "A1"
+    print(A)
+    print(A)
+
+
+# temp = input("猜一下数字：");
+# guess = int(temp)
+# if guess == 8:
+#  print("猜对了")
+# else:
+#     print("猜错了")
+# print("结束")
